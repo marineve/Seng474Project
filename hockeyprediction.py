314d60da1b0db66e99278bb29a5f45ee62137454
Marin Evergreen <marinrevergreen@gmail.com>
2017-03-29 03:13:01 -0700

    Modified code to acquire data and fixed naive bayes alg
    Modified the hockeyprediction script to accept two csv files: one with testing data, and one with data to be classified. The HockeyBayes class changes the data into a usable form by adding discrete attribute values. Modified the implementation of the naive bayes algorithm to work properly.

--- hockeyprediction.py
+++ hockeyprediction.py
@@ -3,36 +3,23 @@
 import naivebayes as nb
 import sys
 
-data_file = 'season_data2.xlsx'
-
-def get_data():
-    return [
-        ['Points', '+/-', 'PP%', 'CORSI%', 'Top 20', 'Playoff Finish'],
-        ['8', '8', '8', '8', '8', '0'],
-        ['2', '2', '2', '2', '2', '4'],
-        ['1', '1', '1', '1', '1', 'SC'],
-        ['4', '4', '4', '4', '4', '1'],
-        ['5', '5', '5', '5', '5', '2'],
-        ['6', '6', '6', '6', '6', '3']
-    ]
 
 def main(argv):
-    # Assign user-input values
-    instance = argv[0]
-    class_value = argv[1]
-    if class_value in nb.classes[1]:
-        # Create a HockeyBayes instance
-        data = get_data()
-        predictor = nb.HockeyBayes(data, instance)
+    # Assign user-input files for testing and classification
+    test_data = argv[0]
+    class_data = argv[1]
 
-        # Find probabilities
-        probability = predictor.find_prob_class_given_evidence(class_value)
-
-        # Output the probability to the user
-        print probability
+    # Create a HockeyBayes instance and classify data
+    predictor = nb.HockeyBayes(test_data)
+    predictor.classify_data(class_data)
 
 if __name__ == "__main__":
+    # Command line arguments:
+    #   testing_data: csv file containing test data
+    #   class_data: the data file to be classified
+    #   result: the result file containing team name and standing
     if len(sys.argv) == 3:
         main(sys.argv[1:])
     else:
+        print 'Two files are required: one for testing, and one for classifying'
         exit(-1)
--- naivebayes.py
+++ naivebayes.py
@@ -1,44 +1,240 @@
-attributes = ('Points', '+/-', 'PP%', 'CORSI%', 'Top 20')
-classes = ('Playoff Finish', ['0', '1', '2', '3', '4', 'SC'])
+import csv
 
-#TODO: Have not accounted for thresholds yet
+attributes = ['points', '+/-', 'pp%', 'corsi%', 't20', 'ploffs']
+classes = ['DNQ', 'R1w', 'R2w', 'R3w', 'R1e', 'R2e', 'R3e', 'SCL', 'SCW']
+num_attribute_values = {
+    'points': 6,
+    '+/-': 7,
+    'pp%': 2,
+    'corsi%': 5,
+    't20': 4,
+    'ploffs': len(classes),
+}
+
 class HockeyBayes:
-    def __init__(self, data, instance):
-        self._data = data
-        self._instance = instance
-        self._prob_list = []
+    """This class uses Naive Bayes algorithm to classify hockey statistics."""
+    def __init__(self, test_data):
+        """Initilizes a HockeyBayes instance.
 
-    def find_prob_class_given_evidence(self, desired_class):
-        for class_val in classes[1]:
+        Args:
+            test_data (file): A csv file containing testing data.
+        """
+        self._test_data = self.get_csv_data(test_data)
+        self._test_data = self.transform_data(self._test_data)
+
+    def classify_data(self, data, result_file_name=None):
+        """Classify data with the testing data.
+
+        Args:
+            data (file): A csv file containing the data to be classified.
+            result_file_name (str): An optional result file name.
+        """
+        instance_data = self.transform_data(self.get_csv_data(data))
+
+        # Set up the results list
+        results = [['team']]
+        results[0].extend(classes)
+
+        # Classify data
+        for instance in instance_data:
+            # Send all data except the last column, which contains what
+            # we're attempting to classify
+            probabilities = self.find_prob_class_given_evidence(instance[:-1])
+
+            # Add the team name
+            all_probs = [instance[0]]
+
+            # Append the probabilities and add it to the results list
+            for val in classes:
+                all_probs.append(probabilities[val])
+            results.append(all_probs)
+
+        # Write the results to a csv file
+        result_csv = self.write_csv_data(results, result_file_name)
+
+    def get_csv_data(self, data_file):
+        """Extract data from a csv file.
+
+        Args:
+            data_file: File containing data in csv format.
+
+        Returns:
+            list: A list of lists containing hockey data
+        """
+        with open(data_file, 'rb') as f:
+            reader = csv.reader(f)
+            return list(reader)
+
+    def write_csv_data(self, data, file_name):
+        """Write data to a csv file.
+
+        Args:
+            data (list): A list of lists to write to a file.
+        """
+        if file_name is None:
+            file_name = 'result.csv'
+        with open(file_name, 'wb') as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
+
+    def transform_data(self, data):
+        """Transform data into usable form using thresholds.
+
+        Args:
+            data (list): A list of lists containing raw data.
+
+        Returns:
+            list: A list of lists containing the processed data.
+        """
+        team_index = data[0].index('team')
+        points_index = data[0].index('points')
+        gf_index = data[0].index('gf')
+        ga_index = data[0].index('ga')
+        pp_percent_index = data[0].index('pp%')
+        corsi_percent_index = data[0].index('corsi%')
+        t20_index = data[0].index('t20')
+        ploffs_index = data[0].index('ploffs')
+
+        processed_data = []
+        for instance in data[1:]:
+            # Replace values with threshold values
+            # Transform Points
+            points_val = float(instance[points_index])
+            if points_val < 70:
+                points = '<70'
+            elif 70 <= points_val < 80:
+                points = '>=70'
+            elif 80 <= points_val < 90:
+                points = '>=80'
+            elif 90 <= points_val < 100:
+                points = '>=90'
+            elif 100 <= points_val:
+                points = '>=100'
+
+            # Find +/-
+            goals_diff_val = float(instance[gf_index]) - float(instance[ga_index])
+            if goals_diff_val < -40:
+                goals_diff = '<-40'
+            elif -40 <= goals_diff_val < -20:
+                goals_diff = '>=-40'
+            elif -20 <= goals_diff_val < 0:
+                goals_diff = '>=-20'
+            elif 0 <= goals_diff_val < 20:
+                goals_diff = '>=0'
+            elif 20 <= goals_diff_val < 40:
+                goals_diff = '>=20'
+            elif 40 <= goals_diff_val < 60:
+                goals_diff = '>=40'
+            elif 60 <= goals_diff_val:
+                goals_diff = '>=60'
+
+            # Power play percentage
+            pp_percent_val = float(instance[pp_percent_index])
+            if pp_percent_val < 20:
+                pp_percent = '<20'
+            else:
+                pp_percent = '>=20'
+
+            # corsi%
+            corsi_percent_val = float(instance[corsi_percent_index])
+            if corsi_percent_val < 46:
+                corsi_percent = '<46'
+            elif 46 <= corsi_percent_val < 49:
+                corsi_percent = '>=46'
+            elif 49 <= corsi_percent_val < 52:
+                corsi_percent = '>=49'
+            elif 52 <= corsi_percent_val < 55:
+                corsi_percent = '>=52'
+            elif 55 <= corsi_percent_val:
+                corsi_percent = '>=55'
+
+            # Top 20
+            t20_val = float(instance[t20_index])
+            if t20_val >= 3:
+                t20 = '>=3'
+            else:
+                t20 = t20_val
+
+            processed_data.append(
+                [
+                    instance[team_index],
+                    points,
+                    goals_diff,
+                    pp_percent,
+                    corsi_percent,
+                    t20,
+                    instance[ploffs_index]
+                ])
+        return processed_data
+
+    def find_prob_class_given_evidence(self, instance):
+        """Determine an instance's class from testing data.
+
+        Args:
+            instance (list): A list containing data for one team.
+
+        Returns:
+            dict: A dictionary containing probabilities for all class values.
+        """
+        prob_dict = {}
+        for class_val in classes:
             class_prob_list = []
             num_class_inst = self.find_num_inst_of_class(class_val)
-            for attr_val in self._instance:
-                index = self._instance.index(attr_val)
-                val_inst = self.find_prob_attr_given_class(attr_val, index, class_val)
-                class_prob_list.append(val_inst/num_class_inst)
+
+            for attr_val in instance:
+                index = instance.index(attr_val)
+                attr = attributes[index]
+                num_val_inst = self.find_num_attr_given_class(attr_val, index, class_val)
+
+                # Correct for the zero-frequency problem
+                numerator = float(num_val_inst) + 1.0
+                denominator = float(num_class_inst) + float(num_attribute_values[attr])
+                class_prob_list.append(numerator/denominator)
+
+
             # Multiply all elements in list together
             all_probs = reduce(lambda x, y: x*y, class_prob_list)
-            self._prob_list.append(all_probs * float(num_class_inst)/float(len(self._data[1:])))
+            class_prob = (float(num_class_inst) + 1)/(len(self._test_data) + num_attribute_values['ploffs'])
+            temp_prob = all_probs * class_prob
+            prob_dict.update({class_val: temp_prob})
 
-        # Find the probability for the desired class
-        class_index = classes[1].index(desired_class)
-        probability = self._prob_list[class_index]/reduce(lambda x, y: x+y, self._prob_list)
-        probability *= 100
-        return probability
+        # Find probabilities for all classes
+        prob_sum = sum(prob_dict.values())
+        for key, value in prob_dict.iteritems():
+            final_prob = value/prob_sum
+            final_prob *= 100
+            prob_dict[key] = final_prob
+        return prob_dict
 
     def find_num_inst_of_class(self, class_value):
+        """Determine the number of instances of a class in the testing data.
+
+        Args:
+            class_value (str): A string representing the class value to find.
+
+        Returns:
+            float: The number of instances containing a particular class value.
+        """
         num_inst_of_class = 0
-        for inst in self._data[1:]:
-            class_index = self._data[0].index(classes[0])
-            if inst[class_index] == class_value:
+        for inst in self._test_data[1:]:
+            if inst[-1] == class_value:
                 num_inst_of_class += 1
         return num_inst_of_class
 
-    def find_prob_attr_given_class(self, value, index, class_value):
+    def find_num_attr_given_class(self, value, index, class_value):
+        """Determine the number of instances a specified value in the testing data.
+
+        Args:
+            value (str): A string representing the value to match.
+            index (int): The index of the value.
+            class_value (str): A string representing the class value.
+
+        Returns:
+            float: The number of instances that contain the value and the class_value.
+        """
         num_value_in_class = 0
-        for inst in self._data[1:]:
-            class_index = self._data[0].index(classes[0])
-            class_present = inst[class_index] == class_value
+        for inst in self._test_data:
+            class_present = inst[-1] == class_value
             value_present = inst[index] == value
             if class_present and value_present:
                 num_value_in_class += 1
