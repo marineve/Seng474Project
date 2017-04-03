6e4d275e4632c1e63966a6ad3b88d24f46907739
Marin Evergreen <marinrevergreen@gmail.com>
2017-04-02 17:42:49 -0700

    Added the ability to specify a result file name

--- hockeyprediction.py
+++ hockeyprediction.py
@@ -13,6 +13,7 @@ parser.add_argument(
                 '--test',
                 required=True,
                 help='A csv file containing testing data')
+parser.add_argument('--result', help='The name of the result file with .csv ext')
 args = parser.parse_args()
 
 # Assign training data
@@ -21,6 +22,9 @@ training_data = args.train
 # Assign testing data
 testing_data = args.test
 
+# Add the result file name
+result_file_name = args.result
+
 # Create a HockeyBayes instance and classify test data
 predictor = nb.HockeyBayes(training_data)
-predictor.classify_data(testing_data)
+predictor.classify_data(testing_data, result_file_name)
